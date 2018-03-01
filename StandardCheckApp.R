eaba594263d6a8bff29e6012d4ba791a96cb01eb
Henry Holm <hholm@whoi.edu>
2018-03-01 17:28:14 -0500

    Update Standard check script
    updated

--- StandardCheckApp.R
+++ StandardCheckApp.R
@@ -29,18 +29,54 @@ makeStandardTable = function(obj, mz, ppm, rtlow, rthigh) {
   mzlow <- (mz-mzrange)
   mzhigh <- (mz+mzrange)
   
-  #create extract a list of lists of peaks that fit our parameters 
-  peaks <- chromPeaks(object = obj, 
-                      mz = c(mzlow, mzhigh), 
-                      rt = c(seclow, sechigh), 
-                      bySample = TRUE)
+  #make a data frame of our sample names
   
-  #
-  peaksmz <- sapply(peaks, function(x) x[1])
-  peaksrt <- sapply(peaks, function(x) (x[4]/60))
-  peaksintensity <- sapply(peaks, function(x) x[7])
-  data.frame(name = mzXMLfiles, mz = peaksmz, rt = peaksrt, intensity = peaksintensity)
+  samplenames <- gsub(chosenFileSubset,"",mzXMLfiles)
   
+  samplenamesframe <- data.frame(samplenames,samplenumber =
+                                   seq(from=1, to=length(mzXMLfiles)))
+  
+  #create + extract a lists of peaks that fit our parameters 
+  peaks <- chromPeaks(object = centWave, 
+                      mz = c(mzlow, mzhigh),
+                      rt = c(seclow, sechigh))
+  
+  #turn our matrix into a dataframe
+  peaksframe <- as.data.frame(peaks)
+  
+  #pull out the columns we want
+  peaksnumber <- peaksframe[["sample"]]
+  peaksmz <- peaksframe[["mz"]]
+  peaksrt <- (peaksframe[["rt"]]/60)
+  peaksintensity <-format(peaksframe[["into"]], scientific = TRUE)
+  
+  #make them into another dataframe
+  samplevalues <- data.frame(name = peaksnumber,
+                             mz = peaksmz,
+                             rt = peaksrt,
+                             intensity = peaksintensity)
+  
+  #Add the sample names back in
+  merged <- merge(samplevalues, samplenamesframe, by.x="name", by.y= "samplenumber")
+  
+  #Reorder our coulmns so sample name comes seconds
+  reordered <- merged[c(1,5,2,3,4)]
+  
+  #Make everything a character so we can add a page break in <- MAYBE ADD SWITCH FOR THIS
+  
+  ascharacters <- as.data.frame(lapply(reordered, as.character), stringsAsFactors = FALSE)
+  
+ Done <- head(do.call(rbind, by(ascharacters, reordered$name, rbind, "")), -1 )
+  
+ # Done <- reordered
+  
+  colnames(Done)[1] <- "Sample Number"
+  colnames(Done)[2] <- "Sample Name"
+  colnames(Done)[3] <- "m/z"
+  colnames(Done)[4] <- "Retention Time"
+  colnames(Done)[5] <- "Intensity"
+  
+  print(Done)
 }
 
 #Function to create the graph
@@ -59,22 +95,46 @@ makeStandardGraph = function(obj, mz, ppm, rtlow, rthigh) {
   
 }
 
-#Create a data.frame  for each standard
+#Create a value for each standard
 rownames <- c("mz","ppm","rtlow","rthigh")
 DNPPE <- c(875.550487, 2.5, 14, 17)
 DGTSd9 <- c(721.66507, 2.5, 15, 19)
-
-
+OleicAcidd9 <- c(290.30509, 2.5, 8.26, 12.26)
+ArachidonicAcidd11 <- c(314.30200, 2.5, 7.13,11.13)
+d7MG181 <- c(381.37042, 2.5,7.42,11.42)
+LysoPEd7181 <- c(487.3524, 2.5,6.14,10.14)
+waxesterd5 <- c(505.56839, 2.5,17.24,21.24)
+d7LysoPC181 <- c(529.39935, 2.5,6.05,10.05)
+d7DG15181 <- c(605.58444, 2.5,15.57,19.57)
+d7PE15181 <- c(711.56642, 2.5,14.71,18.71)
+GluCerad5 <- c(733.63488, 2.5,15.11, 18.99)
+PCd715181 <- c(753.61337, 2.5, 14.59,18.59)
+d5PG16181 <- c(771.59064, 2.5, 13.12, 17.12)
+d5TG <- c(857.83285, 2.5, 21.26, 25.26)
 
 #Make a list for dropdown
-dropdown <- data.frame(DNPPE, DGTSd9, row.names = rownames )
+dropdown <- data.frame("DNPPE"= DNPPE,
+                       "DGTSd9"= DGTSd9, 
+                       "OleicAcidd9" = OleicAcidd9,
+                       "ArachidonicAcidd11" = ArachidonicAcidd11,
+                       "18_1_d7_MG" = d7MG181,
+                       "18_1_d7_Lyso_PE" = LysoPEd7181,
+                       "wax_ester_d5" = waxesterd5,
+                       "18_1_d7_Lyso_PC" = d7LysoPC181,
+                       "15_0_18_1_d7_DG" = d7DG15181,
+                       "15_0_18_1_d7_PE" = d7PE15181,
+                       "C18_Glucosyl_Ceramide_d5" = GluCerad5,
+                       "15_0_18_1_d7_PC" = PCd715181,
+                       "16_0_18_1_D5_PG" = d5PG16181,
+                       "16_0_18_0_16_0_D5_TG" = d5TG,
+                       row.names = rownames)
 
 
 ### Set Up the UI ###
 
 ui <- shinyUI(fluidPage(
   
-  titlePanel("Standard Checker"),
+  titlePanel("Standard Explorer"),
   
   sidebarLayout(
     
@@ -84,9 +144,6 @@ ui <- shinyUI(fluidPage(
       selectInput(inputId = "list",
                   label = "Existing Standard",
                   choices = colnames(dropdown)),
-      
-      actionButton(inputId = "runexisting",
-                   "Run Using Selected"),
       
       # Input: MZ
       numericInput(inputId = "mz", 
@@ -124,10 +181,14 @@ ui <- shinyUI(fluidPage(
                    step = 0.01,
                    width = NULL),
       
+      #GOOOOOOO!
+      actionButton(inputId = "runtest",
+                   "Run"),
+      verbatimTextOutput("parameters")
       
-      actionButton(inputId = "runnew",
-                   "Run Using Inputs")
     ),
+    
+      #Add click button here.
     
     mainPanel(
       tabsetPanel(
@@ -142,49 +203,56 @@ ui <- shinyUI(fluidPage(
 ### server - The code behind the UI
 server <- function(input, output) {
 
-### Code for Exisiting imputs ###
+### Combinded Code ###
 
-  observeEvent(input$runexisting, {
-    frame <- makeStandardTable(obj = centWave,
-                               mz = dropdown[1,as.vector(input$list)],
-                               ppm = dropdown[2,as.vector(input$list)],
-                               rtlow = dropdown[3,as.vector(input$list)],
-                               rthigh = dropdown[4,as.vector(input$list)])
-    output$table <- renderTable(frame)
-  })
+  observeEvent(eventExpr = input$runtest, {
+  mz <- if(is.na(input$mz) == TRUE){
+            dropdown["mz",as.vector(input$list)]
+        } else {
+          input$mz
+        }
+  ppm <- if(is.na(input$ppm) == TRUE){
+            dropdown["ppm",as.vector(input$list)]
+          } else {
+            input$ppm
+          }
+  rtlow <- if(is.na(input$rtmin) == TRUE){
+            dropdown["rtlow",as.vector(input$list)]
+          } else {
+            input$rtmin
+          }
+  rthigh <- if(is.na(input$rtmax) == TRUE){
+            dropdown["rthigh",as.vector(input$list)]
+          } else {
+            input$rtmax
+          }
   
-  observeEvent(input$runexsiting, {
-    output$plot <- renderPlot(makeStandardGraph(obj = centWave,
-                                                mz = dropdown[1, as.vector(input$list)],
-                                                ppm = dropdown[2,as.vector(input$list)],
-                                                rtlow = dropdown[3,as.vector(input$list)],
-                                                rthigh = dropdown[4,as.vector(input$list)])
-    )
+  output$table <- renderTable(makeStandardTable(obj = centWave,
+                                                mz = mz,
+                                                ppm = ppm,
+                                                rtlow = rtlow,
+                                                rthigh = rthigh
+                                                ),
+                              striped = TRUE,
+                              align = 'l',
+                              width = 400,
+                              digits = 5
+                              )
+  
+  output$plot <- renderPlot(makeStandardGraph(obj = centWave,
+                                              mz = mz,
+                                              ppm = ppm,
+                                              rtlow = rtlow,
+                                              rthigh = rthigh
+                                              )
+                            )
+  output$parameters <- renderText(c('Current Settings','\nm/z =',mz,
+                                    '\nppm =',ppm,
+                                    '\nrtlow =',rtlow,
+                                    '\nrthigh =',rthigh))
   }
   )
-  
-### Code for novel Inputs ###
 
-  # Create a table based on the inputs when you press the buttom
-  observeEvent(input$runnew, {
-    frame <- makeStandardTable(obj = centWave,
-                    mz = input$mz,
-                    ppm = input$ppm,
-                    rtlow = input$rtmin,
-                    rthigh = input$rtmax)
-    output$table <- renderTable(frame)
-  })
-  
-  # Create a plot based on the inputs when you press the buttom
-  observeEvent(input$runnew, {
-    output$plot <- renderPlot(makeStandardGraph(obj = centWave,
-                               mz = input$mz,
-                               ppm = input$ppm,
-                               rtlow = input$rtmin,
-                               rthigh = input$rtmax)
-                              )
-                          }
-              )
 }
 
 
